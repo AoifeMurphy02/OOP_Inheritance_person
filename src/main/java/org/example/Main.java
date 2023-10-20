8fa8a9e6f449dc5d874216166d4850d82c0f9669
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2023-10-20 12:46:18 +0100

    person example for Inheritance

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,38 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
 
         ArrayList<Person> people = new ArrayList();
-        people.add(new Person("ana", 2001));
-        people.add(new Student("dan",2002,"software Development"));
-        people.add(new Instructor("kate",1992, 27000));
-        System.out.println(people);
-    }
+        people.add(new Person("Ana", 2001));
+        people.add(new Student("Dan", 2002, "software Development"));
+        people.add(new Student("Jack", 1999, "History"));
+        people.add(new Student("Sam", 1998, "Games Development"));
+        people.add(new Instructor("kate", 1992, 27000));
+        people.add(new Instructor("Victoria", 1988, 44000));
+        people.add(new Instructor("Finn", 1979, 42000));
+        //System.out.println(people);
 
+        System.out.println("\npeople in ArrayList");
+        for (Person person : people) {
+
+            System.out.println(person.toString());
+        }
+
+        System.out.println("\nStudents in ArrayList");
+
+        for (Person person : people) {
+            if (person instanceof Student) {
+                System.out.println("Name: "+ person.getName() + " Major: "+ ((Student) person).getMajor());
+            }
+        }
+        System.out.println("\nInstructors in ArrayList");
+
+        for (Person person : people) {
+            if (person instanceof Instructor) {
+                System.out.println("Name: "+ person.getName() + " Salary: " + ((Instructor) person).getSalary());
+            }
+
+        }
+    }
 }
